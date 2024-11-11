d457baa8e1fe7cca28ae191d49ad2175cbe345eb
JulesNyom <julesnyom@gmail.com>
2024-11-11 23:37:33 +0100

    Created my TopBar

--- saga/lib/screens/HomePage.dart
+++ saga/lib/screens/HomePage.dart
@@ -6,8 +6,15 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold (
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        
+        backgroundColor: Colors.black,
+        title: const Text('Accueil'),
+        titleTextStyle: const TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
       ),
     );
   }
